6605948a63c1f4362b466c7dccba134aa4cceff4
Tammi Lee <tammilee219@gmail.com>
2019-08-08 16:15:32 -0700

    started portfolio project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio{
+    public class HomeController : Controller{
+        [HttpGet("")]
+        public string Index(){
+            return "This is my Index!";
+        }
+        [HttpGet("projects")]
+        public string Project(){
+            return "These are my projects!";
+        }
+        [HttpGet("contact")]
+        public string Contact(){
+            return "This is my Contact info!";
+        }
+    }
+}
